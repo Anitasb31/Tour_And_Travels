18b36044f5ecdf0103b6c8eb42d0993a60660070
Anita Borse <109409557+Anitasb31@users.noreply.github.com>
2025-06-21 19:19:12 +0530

    Add init_db.py

---
+++ init_db.py
@@ -0,0 +1,21 @@
+import psycopg2
+import os
+
+conn = psycopg2.connect(os.environ['DATABASE_URL'])
+cur = conn.cursor()
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS bookings (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        mobile TEXT,
+        arrival TEXT,
+        departure TEXT,
+        persons INTEGER,
+        children INTEGER,
+        message TEXT
+    )
+''')
+conn.commit()
+conn.close()
+
+print("✅ Bookings table created successfully.")
