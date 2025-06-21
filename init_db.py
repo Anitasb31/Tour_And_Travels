125f8475bb3416b3a7b7f24a1a03cf20a5dca66a
Anita Borse <109409557+Anitasb31@users.noreply.github.com>
2025-06-21 20:40:49 +0530

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,21 +1,21 @@
 import psycopg2
 import os
 
-conn = psycopg2.connect(os.environ['DATABASE_URL'])
-cur = conn.cursor()
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS bookings (
-        id SERIAL PRIMARY KEY,
-        name TEXT,
-        mobile TEXT,
-        arrival TEXT,
-        departure TEXT,
-        persons INTEGER,
-        children INTEGER,
-        message TEXT
-    )
-''')
-conn.commit()
-conn.close()
-
-print("✅ Bookings table created successfully.")
+def create_table():
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS bookings (
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            mobile TEXT,
+            arrival TEXT,
+            departure TEXT,
+            persons INTEGER,
+            children INTEGER,
+            message TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+    print("✅ Bookings table created successfully.")
